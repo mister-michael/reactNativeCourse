0748da84d8fb31891fb9b6544c612bc327432d97
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-05 15:50:13 -0500

    Used ListItem component for user div, used Flatlist component for My Listing and My Messages div - rendering a hard coded array with the color icon name and size properties in each object, used a ListItem componnent for logout div, changed ImageComponent prop in list item to be IconComponent

--- App.js
+++ App.js
@@ -3,16 +3,12 @@ import React from "react";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
 
 export default function App() {
   return (
     <>
-      <Screen>
-        <ListItem
-          title="my title"
-          ImageComponent={<Icon name="email" />}
-        />
-      </Screen>
+      <AccountScreen />
     </>
   );
 }
