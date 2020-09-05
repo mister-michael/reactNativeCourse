1f97314bb2f161f93e04b77b6e19e0a600ee1cca
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-05 15:19:07 -0500

    Screen component in App.js, Icon component inside screen component. Created Icon component which takes a name size bckgcolor and iconColor props, set default values for Icon component

--- App.js
+++ App.js
@@ -1,31 +1,14 @@
 // import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppText from "./app/components/AppText";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import Card from "./app/components/Card";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
+import Screen from "./app/components/Screen";
+import Icon from "./app/components/Icon";
 
 export default function App() {
   return (
     <>
-      {/* <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-      <MaterialCommunityIcons name="email" size={200} color="dodgerblue" />
-      <AppText>I love SHIT</AppText>
-      </View> */}
-      {/* <WelcomeScreen></WelcomeScreen> */}
-      {/* <ListingDetailsScreen /> */}
-      {/* <ViewImageScreen /> */}
-      <MessagesScreen />
+      <Screen>
+        <Icon name="email" size={50} backgroundColor="red" iconColor="white" />
+      </Screen>
     </>
   );
 }
