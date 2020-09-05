a048d086e372b030b138f18a475c27c7b485208c
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-05 16:03:23 -0500

    created ListingScreens component, returns a FlatList which renders Card Components from items in the listings array, the data prop takes the array we are rendering, SafeAreaView does not take horizontal padding so in our Screen component we wrapped childre in a View and passed it the style passed through props

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import Constants from 'expo-constants';
-import { StyleSheet, SafeAreaView, View } from 'react-native';
+import React from "react";
+import Constants from "expo-constants";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 
-function Screen({children, style}) {
-    return (
+function Screen({ children, style }) {
+  return (
     <SafeAreaView style={[styles.screen, style]}>
-        {children}
+      <View style={style}>{children}</View>
     </SafeAreaView>
-    )
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        paddingTop: Constants.statusBarHeight,
-        flex: 1,
-    }
-})
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+});
 export default Screen;
