858cb4a6dc1e3cda1eae25292d6d9a1d3c647b9b
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-06 13:52:15 -0500

    built a reusable AppTextInput component, prefix App to TextInput to distinguish from React Components, {...otherProps} is {...props} in RN, (is ...otherProps also a React method?

--- App.js
+++ App.js
@@ -1,33 +1,21 @@
 // import { StatusBar } from "expo-status-bar";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
 import { TextInput, Text } from "react-native";
+import AppTextInput from "./app/components/AppTextInput";
 
 export default function App() {
-
-  const [firstName, setFirstName] = useState('')
+  const [firstName, setFirstName] = useState("");
 
   return (
     <>
-     <Screen>
-       <Text>{firstName}</Text>
-    <TextInput 
-    maxLength={5}
-    keyboardType="numeric"
-    clearButtonMode="always" //only works on ios
-    secureTextEntry={true} //for boolean props, merely naming the prop sets it to true
-    placeholder="First Name"
-    onChangeText={(text) => setFirstName(text)}
-    style={{
-      borderBottomColor: "#ccc",
-      borderBottomWidth: 1,
-    }}
-    />
-     </Screen>
+      <Screen>
+        <AppTextInput placeholder="Username" icon="email" />
+      </Screen>
     </>
   );
 }
