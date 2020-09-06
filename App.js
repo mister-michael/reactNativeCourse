be3bfbe8549b9a2442ef12c6637a4a7879503968
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-06 13:36:54 -0500

    TextInput component built, tested some properties of the ocmponent, onChange is onChangeText

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 // import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {useState} from "react";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
 import AccountScreen from "./app/screens/AccountScreen";
 import ListingsScreen from "./app/screens/ListingsScreen";
+import { TextInput, Text } from "react-native";
 
 export default function App() {
+
+  const [firstName, setFirstName] = useState('')
+
   return (
     <>
-     <ListingsScreen />
+     <Screen>
+       <Text>{firstName}</Text>
+    <TextInput 
+    maxLength={5}
+    keyboardType="numeric"
+    clearButtonMode="always" //only works on ios
+    secureTextEntry={true} //for boolean props, merely naming the prop sets it to true
+    placeholder="First Name"
+    onChangeText={(text) => setFirstName(text)}
+    style={{
+      borderBottomColor: "#ccc",
+      borderBottomWidth: 1,
+    }}
+    />
+     </Screen>
     </>
   );
 }
