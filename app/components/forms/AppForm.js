261960b06cc5ce4b1f8c1a73e7646f8349eba2d2
Michael Clark <michaelclarknashville@protonmail.com>
2020-09-10 00:53:03 -0500

    fixed it, props in AppForm were not destructured

--- app/components/forms/AppForm.js
+++ app/components/forms/AppForm.js
@@ -1,18 +1,14 @@
 import React from "react";
-import {Formik} from "formik";
+import { Formik } from "formik";
 
-function AppForm(initialValues, onSubmit, validationSchema, children) {
+function AppForm({ initialValues, onSubmit, validationSchema, children }) {
   return (
     <Formik
       initialValues={initialValues}
       onSubmit={onSubmit}
       validationSchema={validationSchema}
     >
-        {()=> (
-            <>
-            {children}
-            </>
-        )}
+      {() => <>{children}</>}
     </Formik>
   );
 }
