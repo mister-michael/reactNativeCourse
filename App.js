04747f6b9610407ff74455f9fa73f5b9a10ced0f
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-20 19:55:21 -0500

    built selectImage function, uses ImagePicker from expo-image-picker to access library, its an async function, try catch sets state with the image uri from result.uri

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import * as ImagePicker from "expo-image-picker";
 import * as Permissions from "expo-permissions";
 
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import Screen from "./app/components/Screen";
+import { Button, Image } from "react-native";
 
 export default function App() {
+
+  const [imageUri, setImageUri] = useState()
   const requestPermission = async () => {
     // const result = Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
     //Same thing
@@ -17,5 +20,19 @@ export default function App() {
     requestPermission();
   }, []);
 
-  return <Screen></Screen>;
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync()
+      if (!result.cancelled)
+      setImageUri(result.uri)
+      
+    } catch (error) {
+      console.log("error reading an image")
+    }
+  }
+
+  return <Screen>
+    <Button title="Select Image" onPress={selectImage}/>
+    <Image source={{ uri: imageUri}} style={{width: 200, height: 200}}/>
+  </Screen>;
 }
