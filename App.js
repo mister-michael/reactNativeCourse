e120fe5da7b42bff9e36355c4fb78d0b9a9822d8
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-20 20:32:46 -0500

    baby steps building the imageInput component, pass imageUri from root to imgInp component, if imageUri we render the image, if no imageUri we render the camera icon, even tho we rounded the corners of our container we have to set overflow to hidden in case the image is bigger than the borDers of the container

--- App.js
+++ App.js
@@ -5,10 +5,10 @@ import * as Permissions from "expo-permissions";
 import ListingEditScreen from "./app/screens/ListingEditScreen";
 import Screen from "./app/components/Screen";
 import { Button, Image } from "react-native";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-
-  const [imageUri, setImageUri] = useState()
+  const [imageUri, setImageUri] = useState();
   const requestPermission = async () => {
     // const result = Permissions.askAsync(Permissions.CAMERA_ROLL, Permissions.LOCATION)
     //Same thing
@@ -22,17 +22,23 @@ export default function App() {
 
   const selectImage = async () => {
     try {
-      const result = await ImagePicker.launchImageLibraryAsync()
-      if (!result.cancelled)
-      setImageUri(result.uri)
-      
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
     } catch (error) {
-      console.log("error reading an image")
+      console.log("error reading an image");
     }
-  }
+  };
 
-  return <Screen>
-    <Button title="Select Image" onPress={selectImage}/>
-    <Image source={{ uri: imageUri}} style={{width: 200, height: 200}}/>
-  </Screen>;
+  return (
+    <Screen>
+      {/* <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+        selectImage={selectImage}
+      /> */}
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
+      <ImageInput imageUri={imageUri}/>
+    </Screen>
+  );
 }
---
+++ app/components/ImageInput.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import { Button, Image } from "react-native";
+import colors from "../config/colors";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+
+function ImageInput({ imageUri }) {
+  return (
+    <>
+      <View style={styles.container}>
+        {!imageUri && (
+          <MaterialCommunityIcons
+            color={colors.medium}
+            name="camera"
+            size={40}
+          />
+        )}
+        {imageUri && <Image source={{ uri: imageUri }} style={styles.image} />}
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: colors.light,
+    borderRadius: 15,
+    height: 100,
+    justifyContent: "center",
+    overflow: "hidden",
+    width: 100,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default ImageInput;
