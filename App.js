01ba9a05bb0aa1f45327acb23f9d058c43fac1da
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-25 23:48:05 -0500

    built an AppNavigator to navigate the screens after authentication, authentication will return the AppNavigator when functional

--- App.js
+++ App.js
@@ -9,11 +9,12 @@ import Screen from "./app/components/Screen";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      <AppNavigator />
     </NavigationContainer>
   );
 }
