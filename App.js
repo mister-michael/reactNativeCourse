4a732562449caf0ce1498f74e644fd5ca1526b98
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-25 23:31:28 -0500

    created navigation folder, created AuthNavigation component which calls a createStackNavigator, created an AuthNavigator component to return three screens inside a navigator hold our welcome/login/register screens. welcomeScreen destructures navigation and passes it to the onPress props of the login and register buttons.

--- App.js
+++ App.js
@@ -3,91 +3,16 @@ import { Button, Text } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { MaterialCommunityIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import Screen from "./app/components/Screen";
-
-const Link = () => {
-  const navigation = useNavigation();
-  return (
-    <Button
-      title="click"
-      //optional second parameter, object with key value pairs
-      //we can access this object in the target Screen using route.params.'key'
-      onPress={() => navigation.navigate("TweetDetails", { id: 1 })}
-    />
-  );
-};
-
-const Tweets = ({ navigation }) => (
-  <Screen>
-    <Text>Tweets </Text>
-    <Link />
-  </Screen>
-);
-const TweetDetails = ({ route }) => (
-  //route is not available to child component
-  //in that case we would useRoute() to get access to the route method
-  //  like we did with useNavigation() above
-  <Screen>
-    <Text>Tweet Details {route.params.id}</Text>
-  </Screen>
-);
-
-const Stack = createStackNavigator();
-const FeedNavigator = () => (
-  <Stack.Navigator
-    //screenOptions is defining the header style for child .Screens that don't have their own options prop defined
-    screenOptions={{
-      headerStyle: { backgroundColor: "dodgerblue" },
-      headerTintColor: "white",
-    }}
-  >
-    <Stack.Screen
-      name="Tweets"
-      component={Tweets}
-      options={{
-        //headerStyle takes a style object
-        headerStyle: { backgroundColor: "tomato" },
-        headerTintColor: "white",
-        headerShown: false,
-        //find more options props in documentation
-        //reactnavigation.org Navigators/createStacknavigator/options
-      }}
-    />
-    <Stack.Screen
-      name="TweetDetails"
-      component={TweetDetails}
-      //options can equal or title, or pass a function which returns an object with a title key
-      //because you want to return an object, wrap the object in parentheses
-      options={({ route }) => ({ title: route.params.id })}
-    />
-  </Stack.Navigator>
-);
-
-const AccountNavigator = () => (
-  <Screen>
-    <Text>Account</Text>
-  </Screen>
-);
-
-const Tab = createBottomTabNavigator();
-const TabNavigator = () => (
-  <Tab.Navigator tabBarOptions={{
-    activeBackgroundColor: 'tomato',
-    activeTintColor: 'white',
-    inactiveBackgroundColor: '#eee',
-    inactiveTintColor: 'black'
-  }}>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
-  </Tab.Navigator>
-);
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <NavigationContainer>
-      <TabNavigator />
+      <AuthNavigator />
     </NavigationContainer>
   );
 }
--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,24 +1,29 @@
 import React from "react";
 import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Button from "../components/Button";
 
-function WelcomeScreen(props) {
+function WelcomeScreen({ navigation }) {
   return (
-    <ImageBackground
-      blurRadius={10}
-      style={styles.background}
-      source={require("../assets/background.jpg")}
-    >
-      <View style={styles.logoContainer}>
-        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-        <Text style={styles.tagline}>Sell What You Don't Need</Text>
-      </View>
-      <View style={styles.buttonsContainer}>
-        <Button title="Login" />
-        <Button title="Register" color="secondary" />
-      </View>
-    </ImageBackground>
+      <ImageBackground
+        blurRadius={10}
+        style={styles.background}
+        source={require("../assets/background.jpg")}
+      >
+        <View style={styles.logoContainer}>
+          <Image
+            style={styles.logo}
+            source={require("../assets/logo-red.png")}
+          />
+          <Text style={styles.tagline}>Sell What You Don't Need</Text>
+        </View>
+        <View style={styles.buttonsContainer}>
+          <Button title="Login" onPress={()=> navigation.navigate("Login")}/>
+          <Button title="Register" color="secondary" onPress={()=> navigation.navigate("Register")}/>
+        </View>
+      </ImageBackground>
   );
 }
 
