0b9285e0586e5366f652588d853c6768448402bd
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-27 23:15:01 -0500

    ListingsScreen -> added error state initially false, if our response from database gives error setError(true), if error is true dom renders text to say 'couldnt load' and displays button to retry with onPress of loadListings to retry the fetch. Also installed react-native-debugger and learned to simulate a slow connection

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,14 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
+import Button from '../components/Button';
 import Card from "../components/Card";
 import colors from "../config/colors";
 import listingsApi from '../api/listings';
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
+import AppText from "../components/Text";
 
 function ListingsScreen({navigation}) {
   const [listings, setListings] = useState()
+  const [error, setError] = useState(false)
 
   useEffect(()=> {
     loadListings();
@@ -16,11 +19,18 @@ function ListingsScreen({navigation}) {
 
   const loadListings = async () => {
     const response = await listingsApi.getListings();
+    if (!response.ok) return setError(true)
+
+    setError(false);
     setListings(response.data)
   }
 
   return (
     <Screen style={styles.screen}>
+      {error && <> 
+        <AppText>Couldn't retrieve the listings.</AppText>
+        <Button title="retry" onPress={loadListings} />
+      </>}
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
