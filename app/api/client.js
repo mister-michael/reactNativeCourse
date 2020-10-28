d4e898d2f8afa7c9fd51eb58e3975c570ecf8bfd
Michael Clark <michaelclarknashville@protonmail.com>
2020-10-27 21:47:54 -0500

    exported apiClient from client.js, created listings.js, imported client to listings, defined endpoint and get call, exported object with fetch methods

--- app/api/client.js
+++ app/api/client.js
@@ -4,8 +4,4 @@ const apiClient = create({
     baseURL:`http://192.168.1.69/api`
 });
 
-apiClient.get('/listings').then(response => {
-    if (!response.ok) {
-        response.problem
-    }
-})
+export default apiClient;
