570e3651d5eacda3468ca57912ce6062ee7aaf64
Michael Clark <michaelclarknashville@protonmail.com>
2020-11-10 15:43:47 -0600

    created a reusable hook called useApi in the hooks foler

--- app/screens/ListingsScreen.js
+++ app/screens/ListingsScreen.js
@@ -1,41 +1,33 @@
 import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
-import ActivityIndicator from '../components/ActivityIndicator';
+import ActivityIndicator from "../components/ActivityIndicator";
 
-import Button from '../components/Button';
+import Button from "../components/Button";
 import Card from "../components/Card";
 import colors from "../config/colors";
-import listingsApi from '../api/listings';
+import listingsApi from "../api/listings";
 import routes from "../navigation/routes";
 import Screen from "../components/Screen";
 import AppText from "../components/Text";
+import useApi from "../hooks/useApi";
 
-function ListingsScreen({navigation}) {
-  const [listings, setListings] = useState()
-  const [error, setError] = useState(false)
-  const [loading, setLoading] = useState(false)
+function ListingsScreen({ navigation }) {
+  const { data: listings, error, loading, request: loadListings } = useApi(
+    listingsApi.getListings
+  );
 
-  useEffect(()=> {
-    loadListings();
+  useEffect(() => {
+    loadListings(1, 2, 3);
   }, []);
-
-  const loadListings = async () => {
-    setLoading(true);
-    const response = await listingsApi.getListings();
-    setLoading(false);
-
-    if (!response.ok) return setError(true)
-
-    setError(false);
-    setListings(response.data)
-  }
 
   return (
     <Screen style={styles.screen}>
-      {error && <> 
-        <AppText>Couldn't retrieve the listings.</AppText>
-        <Button title="retry" onPress={loadListings} />
-      </>}
+      {error && (
+        <>
+          <AppText>Couldn't retrieve the listings.</AppText>
+          <Button title="retry" onPress={loadListings} />
+        </>
+      )}
       <ActivityIndicator visible={loading} />
       <FlatList
         data={listings}
